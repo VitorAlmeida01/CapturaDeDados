1771c6432059cd1b95ec20b1a3fe66d88bb4d873
VitorAlmeida01 <vitor.almeida@sptech.school>
2025-02-27 10:36:52 -0300

    code fixed

--- capturaDados.py
+++ capturaDados.py
@@ -4,11 +4,11 @@ import psutil  # Biblioteca para captura dos recursos computacionais
 
 def obter_dados():
     cpuPercent = psutil.cpu_percent(interval=1)  # Porcentagem em uso do CPU
-    cpuFreq = psutil.cpu_freq()
+    cpuFreq = psutil.cpu_freq().max
 
     diskUsage = psutil.disk_usage('/')
     diskUsageTotal = round(diskUsage.total / (1024**3), 2)  # Total do disco em GB
-    diskPercent = psutil.disk_usage().percent
+    diskPercent = psutil.disk_usage('/').percent # Trocar para discol local C: caso o SO seja windows
 
     memory = psutil.virtual_memory()
 
@@ -20,7 +20,7 @@ def obter_dados():
 
 # Efetua a conexão com o banco de dados
 mydb = mysql.connector.connect(
-    host="192.168.56.1",
+    host="10.18.32.79",
     user="insert_user",
     password="borainserir123",
     database="python"
@@ -38,7 +38,7 @@ diskTotal = dados[5]
 
 cod_maq = input("Digite o código da sua máquina (informe somente números).")
 
-sql2 = "Insert into maquina values (%s, %s, %s, %s, %s)"
+sql2 = "Insert into maquina values (%s, %s, %s, %s)"
 val2 = (cod_maq, ramTotal, cpuTotal, diskTotal )
 mycursor.execute(sql2, val2)
 mydb.commit()
@@ -47,7 +47,7 @@ mydb.commit()
 # Loop que sempre será verdadeiro até que o usuario interrompa
 while True:
     # Perguntar ao usuário qual informação deseja ver
-    user_input = input("Digite 'cpu' para ver dados de CPU, 'ram' para RAM, 'disco' para Disco, 'rede' para Rede ou 'sair' para encerrar: ").lower()
+    # user_input = input("Digite 'cpu' para ver dados de CPU, 'ram' para RAM, 'disco' para Disco, 'rede' para Rede ou 'sair' para encerrar: ").lower()
 
     dados = obter_dados()  # Atribuo a função de captura de dados a uma variável
 
@@ -69,11 +69,13 @@ while True:
   
 
     print("\n---- Dados de RAM ----")
-    print(f"Porcentage de RAM: {ram_percent}GB")
+    print(f"Porcentage de RAM: {ram_percent}")
 
 
     print("\n---- Dados de Disco ----")
-    print(f"Porcentagem do disco: {disk_percent}GB")
+    print(f"Porcentagem do disco: {disk_percent}GB \n")
+
+    print('-------------------------------------------')
 
     # Pausa o código por 2 segundos antes de repetir o loop
     time.sleep(2)
